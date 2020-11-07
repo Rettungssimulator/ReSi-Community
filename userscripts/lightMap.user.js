3dd567e0c6b6d2cf142cd0c24193a4ac5232eb02
DispoOhnePlan <62263314+DispoOhnePlan@users.noreply.github.com>
2020-11-07 21:07:04 +0100

    added lightMap.user.js

---
+++ userscripts/lightMap.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         LightMap
+// @namespace    https://rettungssimulator.online
+// @version      0.1
+// @description  Helle Map im DarkMode
+// @author       DispoOhnePlan
+// @match        https://rettungssimulator.online
+// @match        https://rettungssimulator.online/
+// @grant        GM_addStyle
+// ==/UserScript==
+
+
+GM_addStyle("body.dark .leaflet-tile {filter: none !important;-webkit-filter: none !important;}");
